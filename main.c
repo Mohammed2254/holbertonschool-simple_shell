64b800214417c84ebe016a9b79f4c4c22479032b
MohammedAlabdali <m.a.alabduli@gmail.com>
2025-12-21 15:24:40 +0000

    add to main.c a betty comment

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program does nothing.
+ * It simply returns 0 to indicate successful execution.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    return (0);
+}
 
 int main(void)
 {
