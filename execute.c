b52fb74be856d79b59a64a1295d76c7202ab81ff
iihaad9 <fahadfawaz5@gmail.com>
2025-12-24 15:52:49 +0000

    Handle PATH and avoid fork for invalid commands (Task 4)

--- execute.c
+++ execute.c
@@ -1,39 +1,99 @@
 #include "shell.h"
 
-/**
-* execute_command - Forks and executes a command
-* @argv: The array of command and arguments
-*/
+char *get_path(void)
+{
+    int i = 0;
+
+    while (environ[i])
+    {
+        if (strncmp(environ[i], "PATH=", 5) == 0)
+            return (environ[i] + 5);
+        i++;
+    }
+    return (NULL);
+}
+
+char *find_command(char *cmd)
+{
+    char *path, *path_copy, *dir, *full_path;
+    struct stat st;
+
+    path = get_path();
+    if (!path)
+        return (NULL);
+
+    path_copy = strdup(path);
+    dir = strtok(path_copy, ":");
+
+    while (dir)
+    {
+        full_path = malloc(strlen(dir) + strlen(cmd) + 2);
+        if (!full_path)
+            return (NULL);
+
+        strcpy(full_path, dir);
+        strcat(full_path, "/");
+        strcat(full_path, cmd);
+
+        if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
+        {
+            free(path_copy);
+            return (full_path);
+        }
+
+        free(full_path);
+        dir = strtok(NULL, ":");
+    }
+
+    free(path_copy);
+    return (NULL);
+}
+
 int execute_command(char **argv)
 {
-	pid_t child_pid;
-	int status;
+    pid_t child_pid;
+    int status;
+    char *cmd_path;
+    struct stat st;
 
-	if (argv[0] == NULL)
-		return (0);
+    if (argv[0] == NULL)
+        return (0);
 
-	child_pid = fork();
+    if (strchr(argv[0], '/'))
+    {
+        if (stat(argv[0], &st) != 0)
+        {
+            fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+            return (127);
+        }
+        cmd_path = argv[0];
+    }
+    else
+    {
+        cmd_path = find_command(argv[0]);
+        if (!cmd_path)
+        {
+            fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+            return (127);
+        }
+    }
 
-	if (child_pid == -1)
-	{
-		perror("fork");
-	}
-	else if (child_pid == 0)
-	{
-		/* Child Process */
-		if (execve(argv[0], argv, environ) == -1)
-		{
-			fprintf(stderr, "No such file or directory\n");
-			exit(127);
-		}
-	}
-	else
-	{
-		/* Parent Process */
-		wait(&status);
-		
-		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
-	}
-return (0);
+    child_pid = fork();
+    if (child_pid == -1)
+        return (1);
+
+    if (child_pid == 0)
+    {
+        execve(cmd_path, argv, environ);
+        exit(127);
+    }
+    else
+    {
+        wait(&status);
+        if (WIFEXITED(status))
+            return (WEXITSTATUS(status));
+    }
+
+    return (0);
 }
+
